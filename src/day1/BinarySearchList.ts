4a21cc7d7f15c70e9c07802f11caeb6aac12d520
Andrés Olivé <andresolive@gmail.com>
2022-09-20 11:45:34 +0200

    binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // [lo, hi) --> lo is inclusive, hi is exclusive
 
+  let lo = 0;
+  let hi = haystack.length;
+
+  while (lo < hi) {
+    let middle = Math.floor(lo + (hi - lo) / 2);
+    if (haystack[middle] === needle) return true;
+    else if (haystack[middle] < needle) lo = middle + 1;
+    else hi = middle;
+  }
+  return false;
 }
