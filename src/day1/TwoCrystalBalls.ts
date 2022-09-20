1916d1124b8631d7a22cacf1dd0a88b094b0b1b3
Andrés Olivé <andresolive@gmail.com>
2022-09-20 13:05:07 +0200

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  let n = breaks.length;
+  let jumpAmt = Math.floor(Math.sqrt(n));
+
+  // first ball
+  let i = jumpAmt;
+  for (; i < n; i += jumpAmt) {
+    if (breaks[i]) break;
+  }
+
+  // at this point i = the floor where the first ball broke
+
+  // second ball
+  for (let j = i - jumpAmt; j < n; j++) {
+    if (breaks[j]) return j;
+  }
+
+  return -1;
 }
