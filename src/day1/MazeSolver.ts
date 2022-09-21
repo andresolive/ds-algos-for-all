8245fc160ac4507aebc2d8573e04badf26d82733
Andrés Olivé <andresolive@gmail.com>
2022-09-21 16:08:56 +0200

    maze solver complete

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -35,7 +35,7 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
   path.push(curr);
 
   // 2. recurse
-  for (let i = 0; i < maze.length; i++) {
+  for (let i = 0; i < dir.length; i++) {
     const [x, y] = dir[i];
     if (walk(
       maze,
@@ -58,5 +58,14 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
+  const seen: boolean[][] = [];
+  const path: Point[] = [];
 
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+
+  walk(maze, wall, start, end, seen, path);
+
+  return path;
 }
