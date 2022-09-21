627ea50614049324569fbc1a642d0f685e7d4038
Andrés Olivé <andresolive@gmail.com>
2022-09-21 09:09:14 +0200

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    this.length++;
+    const newNode = { value: item } as Node<T>
+
+    if (!this.tail) {
+      this.head = this.tail = newNode;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
 
-}
-    peek(): T | undefined {
+    if (!this.head) return undefined;
 
-}
+    this.length--;
+
+    const prevHead = this.head;
+    this.head = this.head.next;
+
+    prevHead.next = undefined;
+
+    if (this.length === 0) this.tail = undefined;
+
+    return prevHead.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
