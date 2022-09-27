86a2850c7dcc4bf4adfc550380978d2237a6b30a
Andrés Olivé <andresolive@gmail.com>
2022-09-27 10:30:14 +0200

    doub LL insertAt done

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -34,6 +34,42 @@ export default class DoublyLinkedList<T> {
 
   }
   insertAt(item: T, idx: number): void {
+    // say you have [A] <--> [B] <--> [C] and want to insert the new node [F] btwn B and C
+
+    if (idx > this.length) {
+      throw new Error('cannot insert there');
+    }
+
+    if (idx === this.length) {
+      this.append(item);
+      return;
+    } else if (idx === 0) {
+      this.prepend(item);
+      return;
+    }
+
+    this.length++;
+    // we need to traverse the list
+    let curr = this.head;
+    for (let i = 0; curr && i < idx; i++) {
+      curr = curr.next;
+    }
+
+    curr = curr as Node<T>;
+    // curr represents [C]
+
+    // now that we are at the index where we need to insert:
+    const node = { value: item } as Node<T>;
+
+    // first: attach the new node
+    node.next = curr;
+    node.prev = curr.prev;
+    // second: break the old links
+    curr.prev = node;
+    if (curr.prev) {
+      curr.prev.next = curr;
+    }
+
 
   }
   append(item: T): void {
