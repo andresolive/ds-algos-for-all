1ee52e0442a56c837777137e5ea1e54a1544715f
Andrés Olivé <andresolive@gmail.com>
2022-09-27 12:32:46 +0200

    BTPreOrder works

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case
+  if (!curr) return path;
 
+  // remember the 3 steps in recursion
+  // 1. pre
+  path.push(curr.value);
+  // 2. recurse
+  walk(curr.left, path)
+  walk(curr.right, path)
+
+  // 3. post 
+  // nothing to do
+
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
