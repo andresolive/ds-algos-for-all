e94ede28ca7c96e72db44ba47b600a9b4e38724c
Andrés Olivé <andresolive@gmail.com>
2022-09-27 13:38:41 +0200

    DFSOnBST done

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,21 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+  // 3 base cases:
+  // first 2 are structural checks; third one is a value check
+
+  // have we made it into a point in both subtrees in which we cannot recurse any furhter?
+  // this means, structurally, the trees are the exact same
+  if (a === null && b === null) return true;
+
+  // this one means the trees are structurally not the same (one is a node and the other is null)
+  if (a === null || b === null) return false;
+
+  // this is a value check
+  if (a.value !== b.value) return false;
+
+  // you could replace base cases 2 and 3 with the following line
+  // if (a?.value !== b?.value) return false;
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
+
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,16 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  // if we hit the bottom of the tree and couldn't find the value we're looking for
+  if (!curr) return false;
 
+  // if we find the value 
+  if (curr.value === needle) return true;
+
+  if (curr.value < needle) {
+    return search(curr.right, needle);
+  }
+  return search(curr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
