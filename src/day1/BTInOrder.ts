6be58a4d0274999d7f11fd933b39cc15f133ef1b
Andrés Olivé <andresolive@gmail.com>
2022-09-27 12:38:09 +0200

    BTInOrder works

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,24 @@
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case
+  if (!curr) return path;
+
+  // remember the 3 steps in recursion
+  // 1. pre
+  // nothing to do
+
+  // 2. recurse
+  walk(curr.left, path)
+  path.push(curr.value);
+  walk(curr.right, path)
+
+  // 3. post 
+  // nothing to do
+
+  return path;
+}
+
+
 export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 
 }
