99c3bac280e089cd5c8336a628ae3c6350a74ec3
Andrés Olivé <andresolive@gmail.com>
2022-10-12 11:01:37 -0400

    leet: mergeTwoSortedLists implemented recursive

--- src/leetcode/easy/mergeTwoSortedLists.js
+++ src/leetcode/easy/mergeTwoSortedLists.js
@@ -37,3 +37,16 @@ var mergeTwoLists = function (list1, list2) {
 
     return result;
 };
+
+var mergeTwoListsRecursive = function (list1, list2) {
+    if (!list1) return list2;
+    if (!list2) return list1;
+
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoListsRecursive(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoListsRecursive(list1, list2.next);
+        return list2;
+    }
+};
