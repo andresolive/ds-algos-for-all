305d37ca25f54670014f1d3f3c59b4bb8cb17b0c
Andrés Olivé <andresolive@gmail.com>
2022-10-14 09:00:20 -0400

    leet: pascal's triangle implemented

---
+++ src/leetcode/easy/pascalsTriangle.js
@@ -0,0 +1,22 @@
+var generate = function (numRows) {
+    const result = [];
+    let prevRow = [1];
+    result.push(prevRow);
+    if (numRows < 2) return result;
+
+    let rowsBuilt = 1;
+
+    while (rowsBuilt < numRows) {
+        const newRow = [1];
+        for (let i = 0; i < prevRow.length; i++) {
+            const valAtPlusOne = prevRow[i + 1] ? prevRow[i + 1] : 0;
+            const valToPush = prevRow[i] + valAtPlusOne;
+            newRow.push(valToPush);
+        }
+        result.push(newRow);
+        prevRow = newRow;
+        rowsBuilt++;
+    }
+
+    return result;
+};
