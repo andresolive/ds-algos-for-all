4e2c29bf90e45ef3517de2d5ee7b68b1511c9546
Andrés Olivé <andresolive@gmail.com>
2022-10-22 12:06:44 -0400

    leet: firstUniqueCharInString implemented

---
+++ src/leetcode/easy/firstUniqueCharInString.js
@@ -0,0 +1,14 @@
+var firstUniqChar = function (s) {
+    const dict = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in dict) dict[s[i]]++;
+        else dict[s[i]] = 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (dict[s[i]] === 1) return i;
+    }
+
+    return -1;
+};
