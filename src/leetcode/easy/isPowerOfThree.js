a2937f5f4e0d2efb71973bc093205adeb73e136e
Andrés Olivé <andresolive@gmail.com>
2022-10-23 11:39:13 -0400

    leet: isPowerOfThree implemented

---
+++ src/leetcode/easy/isPowerOfThree.js
@@ -0,0 +1,9 @@
+var isPowerOfThree = function (n) {
+    if (n === 1) return true;
+
+    if (n % 3 === 0) return isPowerOfThree(n / 3);
+
+    return false;
+};
+
+console.log(isPowerOfThree(617673396283947));
