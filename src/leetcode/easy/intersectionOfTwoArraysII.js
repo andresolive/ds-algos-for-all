5d330c3ef5940ff6b2f15a7fb977b216077d62f0
Andrés Olivé <andresolive@gmail.com>
2022-10-23 12:06:08 -0400

    leet: intersectionOfTwoArraysII implemented

---
+++ src/leetcode/easy/intersectionOfTwoArraysII.js
@@ -0,0 +1,35 @@
+var intersect = function (nums1, nums2) {
+    const dict1 = {};
+    const dict2 = {};
+    const result = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums1[i] in dict1) {
+            dict1[nums1[i]]++;
+        } else {
+            dict1[nums1[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        if (nums2[i] in dict2) {
+            dict2[nums2[i]]++;
+        } else {
+            dict2[nums2[i]] = 1;
+        }
+    }
+
+    for (let key in dict1) {
+        let amtToPush = 0;
+        if (key in dict2) {
+            amtToPush = Math.min(dict1[key], dict2[key]);
+
+            while (amtToPush > 0) {
+                result.push(key);
+                amtToPush--;
+            }
+        }
+    }
+
+    return result;
+};
