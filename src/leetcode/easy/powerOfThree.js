1f6312e7106ed6d8b7c4e2106738f3c00803b524
Andrés Olivé <andresolive@gmail.com>
2022-10-25 10:49:30 -0400

    leet: powerOfThree explanation

--- src/leetcode/easy/powerOfThree.js
+++ src/leetcode/easy/powerOfThree.js
@@ -1,12 +1,16 @@
-var isPowerOfThree = function (n) {
-    if (n === 1) return true;
-    if (n === 2) return false;
+/* keep dividing n by b as long as the remainder is 0
+this is because n = b^x or n = b * b * ... * b
+hence it should be possible to divide n by b x number of times,
+every time with a remainder of 0 and the end result be 1 */
 
-    let x = 1;
-    while (x < n) {
-        if (n === 3 ** x) return true;
-        x++;
+var isPowerOfThree = function (n) {
+    if (n < 1) {
+        return false;
     }
 
-    return false;
+    while (n % 3 == 0) {
+        n /= 3;
+    }
+
+    return n == 1;
 };
