c969886add5ecccd2e15bfaf7e30d024d11f5803
Andrés Olivé <andresolive@gmail.com>
2022-10-25 12:22:07 -0400

    leet: mergeSortedArrays implemented

---
+++ src/leetcode/easy/mergeSortedArrays.js
@@ -0,0 +1,23 @@
+var merge = function (nums1, m, nums2, n) {
+    let i = 0;
+    let j = 0;
+
+    // remove zeroes from nums1
+    for (let i = 0; i < n; i++) {
+        nums1.pop();
+    }
+
+    while (nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            i++;
+        } else if (nums1[i] > nums2[j]) {
+            const removed = nums2.shift();
+            nums1.splice(i, 0, removed);
+            i++;
+        } else {
+            const removed = nums2.shift();
+            nums1[i] = removed;
+            i++;
+        }
+    }
+};
