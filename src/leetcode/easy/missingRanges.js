85c0adf781af66d41ea7159f9d7cf07d220b3c50
Andrés Olivé <andresolive@gmail.com>
2022-10-25 16:14:06 -0400

    leet: missingRanges implemented

---
+++ src/leetcode/easy/missingRanges.js
@@ -0,0 +1,29 @@
+var findMissingRanges = function (nums, lower, upper) {
+    if (!nums.length && lower === upper) return [`${upper}`];
+    if (!nums.length) return [`${lower}->${upper}`];
+
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (lower < nums[i]) {
+            if (lower === nums[i] - 1) {
+                result.push(`${lower}`);
+                // lower = nums[i] + 1;
+            } else {
+                result.push(`${lower}->${nums[i] - 1}`);
+                // lower = nums[i] + 1;
+            }
+        }
+        lower = nums[i] + 1;
+    }
+
+    if (nums[nums.length - 1] !== upper) {
+        if (nums[nums.length - 1] + 1 === upper) {
+            result.push(`${upper}`);
+        } else {
+            result.push(`${nums[nums.length - 1] + 1}->${upper}`);
+        }
+    }
+
+    return result;
+};
